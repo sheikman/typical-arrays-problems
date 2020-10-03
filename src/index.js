a0ea25a25724fc08ed87452cd39a8c462b54d723
Dima Sheikman <dsheikman@gmail.com>
2020-10-03 16:58:45 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (!arr || !arr.length) {
+    return 0;
+  }
+  return arr.reduce((acc, i) => {
+    if (!acc) {
+      acc = i;
+    }
+
+    if (i < acc) {
+      acc = i
+    }
+
+    return acc;
+  }, null);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (!arr || !arr.length) {
+    return 0;
+  }
+  return arr.reduce((acc, i) => {
+    if (!acc) {
+      acc = i;
+    }
+
+    if (i > acc) {
+      acc = i
+    }
+
+    return acc;
+  }, null);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (!arr || !arr.length) {
+    return 0;
+  }
+  const sum = arr.reduce((acc, i) => acc + i);
+  return sum / arr.length;
 }
